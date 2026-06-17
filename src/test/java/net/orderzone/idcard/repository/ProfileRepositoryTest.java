1c29be7afaafc50be808d671b544b451b71b3f18
La Vireak <e20251458@gic.itc.edu.kh>
2026-06-17 16:41:09 +0700

    feat: configure SQLite for testing and add Ansible playbook for deployment automation (Question 3)

--- src/test/java/net/orderzone/idcard/repository/ProfileRepositoryTest.java
+++ src/test/java/net/orderzone/idcard/repository/ProfileRepositoryTest.java
@@ -6,6 +6,7 @@ import net.orderzone.idcard.model.ProfileType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -18,6 +19,7 @@ import static org.assertj.core.api.Assertions.*;
 
 @DataJpaTest
 @ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class ProfileRepositoryTest {
 
     @Autowired
